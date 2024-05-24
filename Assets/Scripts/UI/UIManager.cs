291023f7107f6767ee823b3f57624043dae41d3b
keano0102 <keano.pittevils@student.arcadiascholen.be>
2024-05-24 13:22:09 +0200

    khad ni alles gesaved

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,18 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Singleton instance
+    public static UIManager Instance { get; private set; }
+
+    [Header("Documents")]
+    public GameObject HealthBar;
+    public GameObject Messages;
+
+    private HealthBarController healthBarController;
+    private Messages messagesController;
+
+    private void Awake()
     {
-        
+        // Implement singleton pattern
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        // Get the script components from the GameObjects
+        if (HealthBar != null)
+        {
+            healthBarController = HealthBar.GetComponent<HealthBarController>();
+        }
+        if (Messages != null)
+        {
+            messagesController = Messages.GetComponent<Messages>();
+        }
+    }
+
+    public void UpdateHealth(int current, int max)
+    {
+        if (healthBarController != null)
+        {
+            healthBarController.SetValues(current, max);
+        }
+    }
+
+    public void AddMessage(string message, Color color)
+    {
+        if (messagesController != null)
+        {
+            messagesController.AddMessage(message, color);
+        }
     }
 }
